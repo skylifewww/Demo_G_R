71e8ff87e07fb02a09207f17d75df5374a5d0a35
Vladimir Nybozhinsky <vladimirnybozhinsky@Mac-mini-Vladimir.local>
2018-03-01 13:20:42 +0200

    paginations

--- DemoAppGetRepos/Controllers/ReposViewController.swift
+++ DemoAppGetRepos/Controllers/ReposViewController.swift
@@ -22,6 +22,7 @@ class ReposViewController: UIViewController, UITableViewDelegate, UITableViewDat
 
     var repos: [GithubRepo] = [GithubRepo]()
     var ImageCache = [String:UIImage]()
+    var isLoadingNewRepos = true
     
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -39,23 +40,38 @@ class ReposViewController: UIViewController, UITableViewDelegate, UITableViewDat
         } else {
             tableView.addSubview(refresher)
         }
-        
+        tableView.register(UINib(nibName: "ActivityIndicatorCell", bundle: nil), forCellReuseIdentifier: "ActivityIndicatorCell")
         
         doSearch(offset: 0)
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if isLoadingNewRepos{
+            return 2
+        }
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repos.count
+        
+        return section == 0 ? repos.count : 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RepoCell") as! RepoCell
-        cell.repo = repos[indexPath.row]
+        
+        if indexPath.section == 1{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ActivityIndicatorCell") as? ActivityIndicatorCell
+            cell?.activityIndicator.startAnimating()
+            return cell!
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RepoCell") as? RepoCell
+        cell?.repo = repos[indexPath.row]
         
         let ownerAvatarURL = repos[indexPath.row].ownerAvatarURL
         
         if let ownerAvatarImage = ImageCache[ownerAvatarURL!] {
-            cell.repoImage.image = ownerAvatarImage
+            cell?.repoImage.image = ownerAvatarImage
             
         } else {
             
@@ -65,12 +81,12 @@ class ReposViewController: UIViewController, UITableViewDelegate, UITableViewDat
                     
                     self.ImageCache[ownerAvatarURL!] = image
                     DispatchQueue.main.async {
-                        cell.repoImage.image = image
+                        cell?.repoImage.image = image
                     }
                 }
             }
         }
-        return cell
+        return cell!
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -78,6 +94,7 @@ class ReposViewController: UIViewController, UITableViewDelegate, UITableViewDat
         let repo = repos[indexPath.row]
         
         if repo.htmlUrl != nil {
+
             let webViewVC = self.storyboard?.instantiateViewController(withIdentifier: "WebViewController") as! WebViewController
             webViewVC.urlString = repo.htmlUrl
             self.navigationController?.pushViewController(webViewVC, animated: true)
@@ -86,6 +103,9 @@ class ReposViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         if indexPath.row == repos.count - 1{
+            
+            isLoadingNewRepos = true
+            
             if let id = NSInteger(repos[indexPath.row].id!){
                 
                 loadMoreRepos(offset: id)
@@ -103,6 +123,7 @@ class ReposViewController: UIViewController, UITableViewDelegate, UITableViewDat
                 let deadLine = DispatchTime.now() + .milliseconds(700)
                 DispatchQueue.main.asyncAfter(deadline: deadLine, execute: {
                     self.refresher.endRefreshing()
+                    self.isLoadingNewRepos = false
                 })
             } else {
                 print(error ?? "???")
@@ -120,6 +141,7 @@ class ReposViewController: UIViewController, UITableViewDelegate, UITableViewDat
                 let deadLine = DispatchTime.now() + .milliseconds(700)
                 DispatchQueue.main.asyncAfter(deadline: deadLine, execute: {
                     self.refresher.endRefreshing()
+                    self.isLoadingNewRepos = false
                 })
             } else {
                 print(error ?? "???")
---
+++ DemoAppGetRepos/Models/ActivityIndicatorCell.swift
@@ -0,0 +1,14 @@
+//
+//  ActivityIndicatorCell.swift
+//  DemoAppGetRepos
+//
+//  Created by Vladimir Nybozhinsky on 01.03.2018.
+//  Copyright © 2018 Vladimir Nybozhinsky. All rights reserved.
+//
+
+import UIKit
+
+class ActivityIndicatorCell: UITableViewCell {
+  
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+}
